3899c3feef2c973a371392c9ae084ffabb51e83c
Juan Pablo <Juan Pablo@192.168.1.13>
2021-05-03 14:30:00 -0300

    Update

--- trackandbug/src/main/java/edu/curso/java/proyecto/trackandbug/bo/Tarea.java
+++ trackandbug/src/main/java/edu/curso/java/proyecto/trackandbug/bo/Tarea.java
@@ -7,20 +7,86 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Tarea {
 
 	@Id
-	@GeneratedValue
-	private Long IdTarea;
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long idTarea;
+	
+	@Column(nullable = false, length = 40)
+	private String nombre;
+	private Long duracion;
+	private Long horasAsignadas;
 	
 	@ManyToOne
-	private TipoDeEstado tipoDeEstado;
-	
+	private Proyecto proyecto;
 	@ManyToOne
 	private TipoDeTarea tipoDeTarea;
-	
+	@ManyToOne
+	private TipoDeEstado tipoDeEstado;
+	@ManyToOne
+	private Usuario usuarioEncargado;
+	@ManyToMany
+	private List<Usuario> usuarios= new ArrayList<Usuario>();
+
 	@OneToMany
-	private List<Comentario> comentarios = new ArrayList<Comentario>();
+	@JoinColumn(name = "TAREA_ID")
+	private List<Comentario> comentarios= new ArrayList<Comentario>();
+
+	public Long getIdTarea() {
+		return idTarea;
+	}
+
+	public void setIdTarea(Long idTarea) {
+		this.idTarea = idTarea;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Long getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(Long duracion) {
+		this.duracion = duracion;
+	}
+
+	public Long getHorasAsignadas() {
+		return horasAsignadas;
+	}
+
+	public void setHorasAsignadas(Long horasAsignadas) {
+		this.horasAsignadas = horasAsignadas;
+	}
+
+	public Proyecto getProyecto() {
+		return proyecto;
+	}
+
+	public void setProyecto(Proyecto proyecto) {
+		this.proyecto = proyecto;
+	}
 
 	public TipoDeTarea getTipoDeTarea() {
 		return tipoDeTarea;
@@ -30,13 +96,35 @@ public class Tarea {
 		this.tipoDeTarea = tipoDeTarea;
 	}
 
+	public TipoDeEstado getTipoDeEstado() {
+		return tipoDeEstado;
+	}
+
+	public void setTipoDeEstado(TipoDeEstado tipoDeEstado) {
+		this.tipoDeEstado = tipoDeEstado;
+	}
+
+	public Usuario getUsuarioEncargado() {
+		return usuarioEncargado;
+	}
+
+	public void setUsuarioEncargado(Usuario usuarioEncargado) {
+		this.usuarioEncargado = usuarioEncargado;
+	}
+
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
 	public List<Comentario> getComentarios() {
 		return comentarios;
 	}
 
 	public void setComentarios(List<Comentario> comentarios) {
 		this.comentarios = comentarios;
-	}
-	
-	
+	}	
 }
--- trackandbug/src/main/java/edu/curso/java/proyecto/trackandbug/bo/TipoDeEstado.java
+++ trackandbug/src/main/java/edu/curso/java/proyecto/trackandbug/bo/TipoDeEstado.java
@@ -2,19 +2,56 @@ package edu.curso.java.proyecto.trackandbug.bo;
 
 import javax.persistence.*;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
 public class TipoDeEstado {
 
 	@Id
-	@GeneratedValue
-	private Long id;
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long idTipoDeEstado;
 	private String nombre;
+	private String descripcion;
+	
+	@OneToOne
+	private TipoDeEstado tipoDeEstado;
+	
+	
+	public Long getIdTipoDeEstado() {
+		return idTipoDeEstado;
+	}
+
+	public void setIdTipoDeEstado(Long idTipoDeEstado) {
+		this.idTipoDeEstado = idTipoDeEstado;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public TipoDeEstado getTipoDeEstado() {
+		return tipoDeEstado;
+	}
+
+	public void setTipoDeEstado(TipoDeEstado tipoDeEstado) {
+		this.tipoDeEstado = tipoDeEstado;
+	}	
 
 	public Long getId() {
-		return id;
+		return idTipoDeEstado;
 	}
 
 	public void setId(Long id) {
-		this.id = id;
+		this.idTipoDeEstado = id;
 	}
 
 	public String getNombre() {
@@ -24,6 +61,49 @@ public class TipoDeEstado {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((descripcion == null) ? 0 : descripcion.hashCode());
+		result = prime * result + ((idTipoDeEstado == null) ? 0 : idTipoDeEstado.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((tipoDeEstado == null) ? 0 : tipoDeEstado.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TipoDeEstado other = (TipoDeEstado) obj;
+		if (descripcion == null) {
+			if (other.descripcion != null)
+				return false;
+		} else if (!descripcion.equals(other.descripcion))
+			return false;
+		if (idTipoDeEstado == null) {
+			if (other.idTipoDeEstado != null)
+				return false;
+		} else if (!idTipoDeEstado.equals(other.idTipoDeEstado))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (tipoDeEstado == null) {
+			if (other.tipoDeEstado != null)
+				return false;
+		} else if (!tipoDeEstado.equals(other.tipoDeEstado))
+			return false;
+		return true;
+	}
 	
 }
